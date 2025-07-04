dd209f6847d3a8be74eac14bae4cd65940108b5a
YUVARAJ P <yuvarajraja804@gmail.com>
2025-07-04 23:48:01 +0530

    Create HelloWorld.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.time.LocalDateTime;
+
+public class HelloDevOps {
+    public static void main(String[] args) {
+        System.out.println("DevOps Build");
+        System.out.println("Current Time: " + LocalDateTime.now());
+    }
+}
