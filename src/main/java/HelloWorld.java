2e2a98c03f11e9ea066070b9333b84f629671a2e
YUVARAJ P <yuvarajraja804@gmail.com>
2025-07-05 00:24:12 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("DevOps Build");
         System.out.println("Current Time: " + LocalDateTime.now());
+        System.out.println("Automation makes software delivery faster and reliable.");
     }
 }
