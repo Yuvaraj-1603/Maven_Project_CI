1850c1ba344ec5096117df9db238ee2cb22208ef
YUVARAJ P <yuvarajraja804@gmail.com>
2025-07-05 00:16:32 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example;
 
 import java.time.LocalDateTime;
 
-public class HelloDevOps {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("DevOps Build");
         System.out.println("Current Time: " + LocalDateTime.now());
